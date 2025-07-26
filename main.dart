d844e6eb861714ec6c5d3482e51b67bf97c91aa4
safwan sanober <safwansanober54@gmail.com>
2025-07-26 12:56:54 +0500

    map

--- main.dart
+++ main.dart
@@ -1,9 +1,52 @@
+import 'dart:io';
+
 void main() {
-  Map stdData = {
+  /* Map<String, dynamic> stdData = {
     "name": "mehak",
     "class": "matric",
     "age": "18",
     "mob number": "033231123333",
+    "marks": [10, 20],
+    "subject": "eng",
   };
   print(stdData);
+  print(stdData.isEmpty);
+  print(stdData.isNotEmpty);
+  print(stdData.length);
+  print(stdData.keys.toList());
+  print(stdData);
+  stdData.addAll({"adress": "abc"});
+  print(stdData);
+  stdData.putIfAbsent("fname", () => "fatima");
+  //stdData.clear();
+  //print(stdData);
+  stdData["percent"] = 98.99;
+  print(stdData);
+  stdData.remove("fname");
+  print(stdData);
+  print(stdData.keys.toList());
+  print(stdData.keys.toList()[4]);*/
+
+  Map<String, dynamic> stdData = {};
+  print(stdData);
+  print("enter your name");
+  String? name = stdin.readLineSync();
+  print(name);
+  print("enter your age");
+  int age = int.parse(stdin.readLineSync()!);
+  print(age);
+  print("enter your marks");
+  int marks = int.parse(stdin.readLineSync()!);
+  print(marks);
+  print("enter your percentage");
+  double percentage = double.parse(stdin.readLineSync()!);
+  print(percentage);
+  stdData.addAll({
+    "name": "name",
+    "age": "age",
+    "marks": "marks",
+    "percentage": "percentage",
+  });
+  print(stdData);
+  
 }
